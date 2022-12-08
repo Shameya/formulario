c0573ebbcc7e007345aed3a982f69bc2a833dc7c
Shameya <117786536+Shameya@users.noreply.github.com>
2022-12-07 23:52:53 -0600

    Add files via upload

---
+++ Producto.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.manejo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Producto {
+    
+private String nombre;
+private String precio;
+
+public Producto() {
+
+}
+public Producto (String_nombre, String_precio) {
+
+}
+public void setNombre(String_nombre) {
+this.nombre = _nombre;
+}
+
